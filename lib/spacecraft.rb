fa5124d497dbeca9f81391d87b539a653cf38886
Marla Schulz <marlaa.schulz@gmail.com>
2021-06-03 09:30:27 -0600

    Add requirement method to class

--- lib/spacecraft.rb
+++ lib/spacecraft.rb
@@ -1,7 +1,12 @@
 class Spacecraft
-  attr_reader :name, :fuel
+  attr_reader :name, :fuel, :requirements
   def initialize (stats)
     @name = stats[:name]
     @fuel = stats[:fuel]
+    @requirements = []
+  end
+
+  def add_requirement(requirement)
+    @requirements << requirement
   end
 end
